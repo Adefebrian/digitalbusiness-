8eee2763274700081ba27cc6fad57ab72b83e83f
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2023-05-16 17:23:36 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ import { getRandomSeed } from "lib/seeds";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export const appName = "Paint by Text";
-export const appSubtitle = "Edit your photos using written instructions, with the help of an AI.";
-export const appMetaDescription = "Edit your photos using written instructions, with the help of an AI.";
+export const appName = "Smartcrypt";
+export const appSubtitle = "Coming soon....";
+export const appMetaDescription = "Coming soon....";
 
 export default function Home() {
   const [events, setEvents] = useState([]);
